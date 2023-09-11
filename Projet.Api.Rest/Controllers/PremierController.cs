ebd863036f31d5b73e4789b8df173ffa55121c18
Aziz Ourabiah <ourabiahaziz1@gmail.com>
2023-09-10 20:07:45 -0400

    First commit

---
+++ Projet.Api.Rest/Controllers/PremierController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projet.Api.Rest.Models;
+
+namespace Projet.Api.Rest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremierController : ControllerBase
+    {
+
+        //private readonly DatabaseContext _context;
+
+        //public PremierController(DatabaseContext context)
+        //{
+        //    //injection de dépendances
+        //    _context = context;
+        //}
+        
+        //[HttpGet]
+        //public async Task<ActionResult<List<ItemModel>>> Get()
+        //{
+        //    var result = await _context.ItemModels.ToListAsync();
+        //    return Ok(result);
+        //    //return Ok(items);
+        //}
+
+        //[HttpGet("{name}")]
+        //public async Task<ActionResult<ItemModel>> Get(string name)
+        //{
+        //    var item = await _context.ItemModels.FindAsync(name);
+        //    if (item == null) 
+        //        return BadRequest("pas d'item trouvé !");
+
+        //    return Ok(item);
+        //}
+
+        //[HttpPost]
+        //public async Task<ActionResult<ItemModel>> Post(ItemModel model)
+        //{
+        //    _context.ItemModels.Add(model);
+        //    await _context.SaveChangesAsync();
+
+        //    //items.Add(model);
+        //    return CreatedAtAction("POST", model);
+        //}
+
+        //[HttpPut]
+        //public async Task<ActionResult<ItemModel>> Put(string name,ItemModel model)
+        //{
+        //    if (model.Name != name)
+        //        return BadRequest("L'élément n'existe pas");
+
+
+        //    _context.Entry(model).State = EntityState.Modified;
+
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok(model);    
+        //}
+
+        //[HttpDelete]
+        //public async Task<ActionResult<ItemModel>> Delete(string name)
+        //{
+        //    var item = await _context.ItemModels.FindAsync(name);
+        //    if (item == null)
+        //        return BadRequest("pas d'item trouvé !");
+
+        //    _context.ItemModels.Remove(item);
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok(item);
+        //}
+    }
+}
---
+++ Projet.Api.Rest/Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projet.Api.Rest.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        //Déclarations des datatables...
+        public DbSet<ItemModel> ItemModels { get; set; } = null!;
+    }
+}
